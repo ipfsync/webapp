82e03a51f78cda8a8b5a3043a5a81383875d5a89
Xiaoyu Huang <007pig@gmail.com>
2019-05-15 11:25:19 +0800

    Working on websocket client

---
+++ sources/models/websocket.ts
@@ -0,0 +1,16 @@
+
+export default class WSClient {
+	private readonly socket: WebSocket;
+	constructor() {
+		this.socket = new WebSocket('ws://localhost:8080');
+		this.socket.addEventListener('open', function (event) {
+			console.log("Websocket connected.")
+		});
+
+		this.socket.addEventListener('message', function (event) {
+			console.log('Message from server ', event.data)
+		});
+	}
+
+}
+
